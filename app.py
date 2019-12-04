bd34c56f338c42882033b6e7a0ca5bdbaa28e5d2
ProdigyX6217 <dominic.holder.15@cityas.org>
2019-12-04 02:12:13 -0800

    Users can see comments

--- app.py
+++ app.py
@@ -5,15 +5,12 @@ import os
 
 app = Flask(__name__)
 
-# client = MongoClient()
-# db = client.Playlister
-# playlists = db.playlists
-
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
 client = MongoClient(host=host)
 # client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.get_default_database()
 playlists = db.playlists
+comments = db.comments
 
 def video_url_creator(id_lst):
     videos = []
@@ -22,6 +19,7 @@ def video_url_creator(id_lst):
         video = 'https://youtube.com/embed/' + vid_id
         videos.append(video)
     return videos
+
 
 @app.route('/')
 def playlists_index():
@@ -56,6 +54,7 @@ def playlists_submit():
 def playlists_show(playlist_id):
     """Show a single playlist."""
     playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
+    playlist_comments = comments.find({'playlist_id': ObjectId(playlist_id)})
     return render_template('playlists_show.html', playlist=playlist)
 
 
@@ -91,6 +90,25 @@ def playlists_delete(playlist_id):
     """Delete one playlist."""
     playlists.delete_one({'_id': ObjectId(playlist_id)})
     return redirect(url_for('playlists_index'))
+
+# Add this header to distinguish Comment routes from Playlist routes
+########## COMMENT ROUTES ##########
+
+@app.route('/playlists/comments', methods=['POST'])
+def comments_new():
+    """Submit a new comment."""
+    # TODO: Fill in the code here to build the comment object,
+    # and then insert it into the MongoDB comments collection
+    comment = {
+        'title': request.form.get('title'),
+        'content': request.form.get('content'),
+        'playlist_id': ObjectId(request.form.get('playlist_id'))
+        'created_at': datetime.now()
+    }
+
+    print(comment)
+    comment_id = comments.insert_one(comment).inserted_id
+    return redirect(url_for('playlists_show', playlist_id=request.form.get('playlist_id')))
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
