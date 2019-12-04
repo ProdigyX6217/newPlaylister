c6f1391de8169c232aadafdfea51966dada5ab22
ProdigyX6217 <dominic.holder.15@cityas.org>
2019-12-03 20:15:25 -0800

    Users can edit, update, and destroy playlists

--- app.py
+++ app.py
@@ -21,10 +21,12 @@ def playlists_index():
     """Show all playlists"""
     return render_template('playlists_index.html', playlists=playlists.find())
 
+
 @app.route('/playlists/new')
 def playlists_new():
     """Create a new playlist."""
     return render_template('playlists_new.html', playlists={}, title='New Playlist')
+
 
 @app.route('/playlists', methods=['POST'])
 def playlists_submit():
@@ -42,16 +44,39 @@ def playlists_submit():
     playlist_id = playlists.insert_one(playlist).inserted_id
     return redirect(url_for('playlists_show', playlist_id = playlist_id))
 
-@app.route('/playlists/<playlist_id>')
+
+@app.route('/playlists/<playlist_id>', methods= ['GET'])
 def playlists_show(playlist_id):
     """Show a single playlist."""
     playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
     return render_template('playlists_show.html', playlist=playlist)
 
+
+@app.route('/playlists/<playlist_id>/edit')
+def playlists_edit(playlist_id):
+    """Show the edit form for a playlist."""
+    playlist = playlists.find_one({'_id': ObjectId(playlist_id), 'video_ids': "\n".join(playlist.video_ids)})
+    return render_template('playlists_edit.html', playlist=playlist, title="Edit Playlist")
+
+
+@app.route('/playlists/<playlist_id>', methods=['POST'])
+def playlists_update(playlist_id):
+    """Submit an edited playlist."""
+    video_ids = request.form.get('video_ids').split()
+    videos = video_url_creator(video_ids)
+    # create our updated playlist
+    updated_playlist = {
+        'title': request.form.get('title'),
+        'description': request.form.get('description'),
+        'videos': videos,
+        'video_ids': video_ids
+    }
+    # set the former playlist to the new one we just updated/edited
+    playlists.update_one(
+        {'_id': ObjectId(playlist_id)},
+        {'$set': updated_playlist})
+    # take us back to the playlist's show page
+    return redirect(url_for('playlists_show', playlist_id=playlist_id))
+
 if __name__ == '__main__':
     app.run(debug=True)
-
-# @app.route('/')
-# def index():
-#     """Return homepage."""
-#     return render_template('home.html', msg='Flask is Cool!!')
