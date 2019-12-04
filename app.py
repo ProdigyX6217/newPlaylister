c8001a4e61a3c48966e72f5eaba5fb5ed47621f1
ProdigyX6217 <dominic.holder.15@cityas.org>
2019-12-04 01:39:35 -0800

    Update ENV VAR Port

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ app = Flask(__name__)
 
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
 client = MongoClient(host=host)
+# client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.get_default_database()
 playlists = db.playlists
 
@@ -92,4 +93,4 @@ def playlists_delete(playlist_id):
     return redirect(url_for('playlists_index'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
